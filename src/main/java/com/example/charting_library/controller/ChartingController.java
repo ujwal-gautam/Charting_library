58dab533bac33c3a6e038cb9209e25b639a9c4b3
ujwal-gautam <ujwalgautam96@gmail.com>
2022-03-28 16:51:19 +0530

    add logger

--- src/main/java/com/example/charting_library/controller/ChartingController.java
+++ src/main/java/com/example/charting_library/controller/ChartingController.java
@@ -1,5 +1,7 @@
 package com.example.charting_library.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,10 @@ import java.util.Map;
 @RestController
 public class ChartingController
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChartingController.class);
     @GetMapping("/list")
     public Map<String, String> getList(){
+        LOGGER.info("getting list...");
         Map<String, String> stringMap = new HashMap<>();
         stringMap.put("1", "Ujwal");
         stringMap.put("2", "Mahesh");
